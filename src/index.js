6e2d60cabe812ec567cab31d01e5876d3681b82d
Ali Alizada <ali.alizada@qileap.com>
2020-02-01 13:35:37 -0800

    add scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './styles/main.scss'
+import './styles/index.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
