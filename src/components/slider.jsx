49fc1d8392cba7fb96307abd7c6784455d7675e0
muhammadwafa <iamprogrammercoder@gmail.com>
2020-02-05 21:41:23 +0430

    slider only added

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-export const Slider = () => {
-  const handleOnDragStart = e => e.preventDefault();
 
+const responsive = {
+  0: { items: 1 },
+  1024: { items: 2 }
+};
+
+export const Slider = () => {
   return (
     <div className="sliderarea">
       <h3>お客様の声</h3>
       <p>たくさんの方々に反響を頂いております。</p>
 
       <div className="slider">
-        <AliceCarousel mouseTrackingEnabled>
+        <AliceCarousel responsive={responsive} mouseTrackingEnabled>
           <div className="card"></div>
           <div className="card"></div>
           <div className="card"></div>
