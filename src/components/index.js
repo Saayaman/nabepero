c73ce7e0b4ba366c85437efe589a7b9e7859ea19
muhammadwafa <iamprogrammercoder@gmail.com>
2020-02-05 22:48:33 -0800

    companies button added

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { About } from "./about";
-export { Header } from "./Header";
+export { Header } from "./header";
 export { Footer } from "./footer";
 export { TopBody } from "./topbody";
 export { MainBody } from "./mainbody";
+export { Companies } from "./companies";
 export { ContactForm } from "./contactform";
