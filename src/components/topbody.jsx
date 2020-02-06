dcbdd1b4296fc57439c5eac3168a3cb9baa5d9ba
Ayako <ayakosayama1987@gmail.com>
2020-02-06 00:05:27 -0800

    update

--- src/components/topbody.jsx
+++ src/components/topbody.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ReactComponent as Budge } from "assets/images/budge.svg";
 import Button from  "./button";
+import Container from "./container";
 
 export const TopBody = () => {
   return (
@@ -9,18 +10,23 @@ export const TopBody = () => {
         <div className="hero-left">
           <h1>海外登記簿の取得、<br />お困りですか？</h1>
 
-          <p>
-            Twitter、Google、２ちゃんねる、５ちゃんねる等。<br />
-            海外法人との裁判に必要な資格証明書の取得を代行いたします
-          </p>
+          <div className="hero-left">
+            <h1>海外登記簿の取得、<br />お困りですか？</h1>
 
-          <Button text={"無料相談する"} />
-        </div>
+            <p>
+              Twitter、Google、２ちゃんねる、５ちゃんねる等。<br />
+              海外法人との裁判に必要な資格証明書の取得を代行いたします
+            </p>
 
-        <div className="hero-right">
-          <Budge />
+            <Button text={"無料相談する"} />
+          </div>
+
+          <div className="hero-right">
+            <Budge />
+          </div>
+          </Container>
+
         </div>
-      </div>
     </section>
   );
 };
