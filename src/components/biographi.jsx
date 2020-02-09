e202496149ee97410456c9cd34a11e601e1e1e94
Ayako <ayakosayama1987@gmail.com>
2020-02-08 19:10:25 -0800

    update

--- src/components/biographi.jsx
+++ src/components/biographi.jsx
@@ -12,43 +12,39 @@ export const BioGraphi = () => {
         <div className="BioGraphi-wrapper">
           <div className="BioGraphi-left">
             <div className="BioGraphi-image"><img src={imgBio} /></div>
-            <div>所属</div>
-            <div>対応言語</div>
+            <div className="BioGraphi-section-text">
+
+              <p><span>所属</span>〇〇法律事務所</p>
+              <p><span>対応言語</span>英語・日本語</p>
+            </div>
           </div>
 
           <div className="BioGraphi-right">
             <div className="BioGraphi-section">
               <h4>経歴</h4>
               <div className="BioGraphi-section-text">
-                2003年	京都大学法学部（法学士）
-                2006年	京都大学大学院法学研究科（法学修士）
-                2007年	最高裁判所司法研修所修了（60期）
-                2007年 - 2018年	西村あさひ法律事務所勤務
-                2013年 - 2014年	金融庁総務企画局市場課 出向
-                2016年	米国Cornell Law School（LL.M.）
-                2016年 - 2017年	香港のHarney Westwood & Riegels法律事務所勤務
-                2018年	当事務所入所
+                <p><span>2003年</span>京都大学法学部（法学士）</p>
+                <p><span>2006年</span>京都大学大学院法学研究科（法学修士）</p>
+                <p><span>2007年</span>最高裁判所司法研修所修了（60期）</p>
+                <p><span>2007-2018年</span>西村あさひ法律事務所勤務</p>
+                <p><span>2013-2014年</span>	金融庁総務企画局市場課 出向</p>
+                <p><span>2016年</span>米国Cornell Law School（LL.M.）</p>
+                <p><span>2016-2017年	</span>香港のHarney Westwood & Riegels法律事務所勤務</p>
+                <p><span>2018年	</span>当事務所入所</p>
               </div>
             </div>
             <div className="BioGraphi-section">
               <h4>著者・メディア</h4>
               <div className="BioGraphi-section-text">
-              投資信託の法制と実務対応 商事法務
-              2015年12月
-              平成25年改正金商法等(1年半以内施行)政府令の解説(2・完)　投資信託・投資法人法制の見直し 旬刊商事法務No.2045（2014年10月5・15日合併号）
-              2014年10月
-              平成25年改正金商法政府令の解説(2)　投資証券等の取引に関するインサイダー取引規制の導入等 旬刊商事法務No.2030（2014年4月15日号）
-              2014年4月
-              Tax on corporate lending and bond issues (Japan Chapter) Practical Law Multi-jurisdictional guide 2013/14 Tax on Transactions
-              2013年4月
-              Tax on Finance Transactions (Japan Chapter) Practical Law Multi-jurisdictional guide 2012/13 Tax on Transactions
-              2012年7月
-              D1-Law.com 要件事実体系 一般民事編 D1-Law.com Website
-              2012年4月
-              Tax on Transactions Handbook (Japan Chapter) The PLC Cross-border Tax on Transactions Handbook 2011/12
-              2011年6月
-              法的紛争処理の税務〔第3版〕(下)会社・倒産・国際税務 民事法研究会
-              2009年7月
+                <p>投資信託の法制と実務対応 商事法務<br/><span>2015年12月</span></p>
+                <p>平成25年改正金商法等(1年半以内施行)政府令の解説(2・完)　投資信託・投資法人法制の見直し 旬刊商事法務No.2045（2014年10月5・15日合併号）<span>2014年10月</span></p>
+                <p>平成25年改正金商法政府令の解説(2)　投資証券等の取引に関するインサイダー取引規制の導入等 旬刊商事法務No.2030（2014年4月15日号）<span>2014年4月</span></p>
+                <p>Tax on corporate lending and bond issues (Japan Chapter) Practical Law Multi-jurisdictional guide 2013/14 Tax on Transaction<span>2013年4月</span></p>
+                <p>Tax on Finance Transactions (Japan Chapter) Practical Law Multi-jurisdictional guide <span>2012/13</span></p>
+                  <p>Tax on Transactions<span>2012年7月</span></p>
+                <p>D1-Law.com 要件事実体系 一般民事編 D1-Law.com Website<span>2012年4月</span></p>
+                <p>Tax on Transactions Handbook (Japan Chapter) The PLC Cross-border Tax on Transactions Handbook 2011/12<span>2011年6月</span></p>
+                <p>法的紛争処理の税務〔第3版〕(下)会社・倒産・国際税務 民事法研究会<span>2009年7月</span></p>
               </div>
             </div>
           </div>
